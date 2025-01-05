6740b0287fc224fc87ffea320d52ef96d1b3ddf6
lyviogd <lyviogd@gmail.com>
2025-01-05 01:21:06 -0400

    Chap 4: creation du feed

---
+++ lib/helper/demo_values.dart
@@ -0,0 +1,10 @@
+class DemoValues {
+  static final String userImage = "assets/images/user.jpg";
+  static final String postImage = "assets/images/leaf.jpg";
+  static final String userName = "Din Javel";
+  static final String userEmail = "testest@gmail.com";
+  static final String postTime = "05/01/25";
+  static final String postTitle = "Maple Trees";
+  static final String postSummary =
+      "A comparative study of maple leaves and other plants";
+}
---
+++ lib/view/presentation/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TextThemes {
+  static const Color primaryColor = Colors.blue;
+
+  static const TextStyle dateStyle = TextStyle(
+      fontSize: 10.0, fontWeight: FontWeight.bold, color: Colors.green);
+
+  static const TextStyle bodyText1 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.normal,
+  );
+}
